0fb1d698ae4761389196154d8da1f1af74c9d41f
Ogün Can KAYA <oguncanlnx@gmail.com>
2020-08-05 10:43:09 +0300

    Home Activity - Camera and Message Fragment Design and Code / Camera45

--- app/src/main/java/com/oguncan/umessage/Home/CameraFragment.kt
+++ app/src/main/java/com/oguncan/umessage/Home/CameraFragment.kt
@@ -20,6 +20,7 @@ class CameraFragment : Fragment() {
         Log.e("Merhaba3","Merhaba3")
         Log.e("Merhaba4","Merhaba4")
         Log.e("Merhaba44","Merhaba44")
+        Log.e("Merhaba45","Merhaba45")
         return view
     }
 }
