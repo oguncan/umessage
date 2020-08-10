d9f53d360fbb839ab685e6bd39514501dab808f1
Ogün Can KAYA <oguncanlnx@gmail.com>
2020-08-10 15:46:13 +0300

    Home Activity - Camera and Message Fragment Design and Code / Camera New Feature

--- app/src/main/java/com/oguncan/umessage/Home/CameraFragment.kt
+++ app/src/main/java/com/oguncan/umessage/Home/CameraFragment.kt
@@ -15,13 +15,7 @@ class CameraFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view = inflater.inflate(R.layout.fragment_camera, container, false)
-        Log.e("Merhaba","Merhaba")
-        Log.e("Merhaba2","Merhaba2")
-        Log.e("Merhaba3","Merhaba3")
-        Log.e("Merhaba4","Merhaba4")
-        Log.e("Merhaba44","Merhaba44")
-        Log.e("Merhaba45","Merhaba45")
-        Log.e("Merhaba50","Merhaba50")
+
         return view
     }
 }
